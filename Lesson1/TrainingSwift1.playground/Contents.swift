2c7020aa7edfa16e2002ec9fe7055932df27c42b
hung070996 <do.tien.hung@framgia.com>
2018-07-05 15:18:20 +0700

    let, var, string

---
+++ Lesson1/TrainingSwift1.playground/Contents.swift
@@ -0,0 +1,36 @@
+
+//import UIKit
+
+//Cac cach khai bao var
+var s1 : Int
+s1 = 1
+print("Result: \(s1)")
+
+var s2 : String = "two"
+print("Result: \(s2)")
+
+var s3 = "three"
+print("Result: \(s3)")
+
+// var co the thay doi gia tri cua bien
+s1 = 3
+s2 += "one"
+s3 = "1"
+if s2 != s3 {
+    print("Result: \(s1), \(s2), \(s3)")
+}
+
+
+//Cac cach khai bao let
+let a1 : String
+a1 = "ten"
+print("Result: \(a1)")
+
+let a2 : Int = 9
+print("Result: \(a2)")
+
+let a3 = 8
+print("Result: \(a3)")
+
+//let ko the thay doi gia tri
+
